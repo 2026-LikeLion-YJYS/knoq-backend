a991df5497dc750ede2d3765bcf36347c1b192ee
seyeon <se020122@naver.com>
2026-08-18 13:15:43 +0900

    feat: 직원 요청 인박스 및 상세 조회 구현 #17

--- knoq/src/main/java/com/knoq/knoq/consultation/repository/ConsultationRequestRepository.java
+++ knoq/src/main/java/com/knoq/knoq/consultation/repository/ConsultationRequestRepository.java
@@ -5,7 +5,10 @@ import com.knoq.knoq.consultation.entity.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface ConsultationRequestRepository extends JpaRepository<ConsultationRequest, String> {
     boolean existsBySessionIdAndStatusIn(String sessionId, Collection<RequestStatus> statuses);
+
+    List<ConsultationRequest> findAllByStoreIdOrderByRequestedAtDesc(Long storeId);
 }
